bd0d6c146b7cf47e6ddb4768829e9779d06406eb
Darren Dang (gmail) <dangphibang@gmail.com>
2026-08-16 16:17:10 -0700

    Make robots metadata route static-export compatible

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,10 @@
-import type {MetadataRoute} from 'next';export default function robots():MetadataRoute.Robots{return {rules:{userAgent:'*',allow:'/'},sitemap:'https://darrendang.com/sitemap.xml'}}
+import type { MetadataRoute } from 'next';
+
+export const dynamic = 'force-static';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://darrendang.com/sitemap.xml',
+  };
+}
